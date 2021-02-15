0807b58398b24e8ef6de5b4dc9788cee4e6ec17e
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-15 13:33:59 -0300

    refactor: move fake HttpRequest without name creation to a factory method

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,17 +1,19 @@
 import { HttpRequest } from '../../protocols/http'
 import { SignUpController } from './signup-controller'
 
+export const makeFakeRequestWithoutName = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
     const sut = new SignUpController()
 
-    const httpRequest: HttpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequestWithoutName()
 
     const httpResponse = await sut.handle(httpRequest)
 
