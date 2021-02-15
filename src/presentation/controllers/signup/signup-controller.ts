8432f713bb004024f23bf3c9eccf66f2f44deeee
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-15 13:49:32 -0300

    refactor: make SignUpController implements Controller interface

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,6 +1,7 @@
+import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     return {
       statusCode: 400
