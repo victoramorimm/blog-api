df4d0c5ee21c1d534b995fae49a94fcb6c3c23c9
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-17 11:37:23 -0300

    feat: ensure DbAddAccount calls Hasher with correct password

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,18 +1,24 @@
+import { Hasher } from '../../domain/protocols/hasher'
 import { AddAccount } from '../../domain/usecases/add-account'
 import { InvalidParamError } from '../../presentation/errors'
 import { badRequest } from '../../presentation/helpers/http'
 import { EmailValidator } from '../../presentation/protocols/email-validator'
 
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly hasher: Hasher
+  ) {}
 
   async add(value: any): Promise<any> {
-    const { email } = value.body
+    const { email, password } = value.body
 
     const isEmailValid = this.emailValidator.validate(email)
 
     if (!isEmailValid) {
       return badRequest(new InvalidParamError('email'))
     }
+
+    await this.hasher.hash(password)
   }
 }
