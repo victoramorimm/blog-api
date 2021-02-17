0f4dca6ce58a72740926168c377164acb028af8a
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-17 10:34:33 -0300

    refactor: make signup-controller use the signup-protocols

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,12 +1,13 @@
-import { Hasher } from '../../../domain/protocols/hasher'
-import { AddAccount } from '../../../domain/usecases/add-account'
-import { InvalidParamError } from '../../errors/invalid-param-error'
-import { MissingParamError } from '../../errors/missing-param-error'
-import { ServerError } from '../../errors/server-error'
+import {
+  Hasher,
+  AddAccount,
+  HttpRequest,
+  HttpResponse,
+  Controller,
+  EmailValidator
+} from './signup-protocols'
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http'
-import { Controller } from '../../protocols/controller'
-import { EmailValidator } from '../../protocols/email-validator'
-import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
   constructor(
