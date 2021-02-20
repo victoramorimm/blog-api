d3c44fcd414ae40e2991f2904211ae46e71a554d
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-19 21:11:23 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountReturnedByDbModel } from '../../domain/models/account-returned-by-db'
+
+export interface LoadAccountByEmailRepository {
+  loadByEmail: (value: string) => Promise<AccountReturnedByDbModel>
+}
--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -2,17 +2,21 @@ import { AccountReturnedByDbModel } from '../../domain/models/account-returned-b
 import { Hasher } from '../../domain/protocols/hasher'
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
 import { EmailValidator } from '../../presentation/protocols/email-validator'
+import { LoadAccountByEmailRepository } from '../protocols/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly emailValidator: EmailValidator,
-    private readonly hasher: Hasher
+    private readonly hasher: Hasher,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountReturnedByDbModel> {
     const { email, password } = accountData
 
     this.emailValidator.validate(email)
+
+    await this.loadAccountByEmailRepository.loadByEmail(email)
 
     await this.hasher.hash(password)
 
