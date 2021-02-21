cfd33c4b96afa990eabb50ac07e60094120ab2a5
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-21 14:36:45 -0300

    refactor: remove some unnecessary files from tests coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/data/protocols/criptography/hasher.ts',
+    '!<rootDir>/src/data/usecases/db-add-account-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
