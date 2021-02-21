23778f5c859951bce7de631872c38b327045f852
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-20 21:20:36 -0300

    test: ensure BcryptAdapter returns a valid hash on success

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return 'hashed_value'
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call hash with correct values', async () => {
     const salt = 12
@@ -12,5 +18,15 @@ describe('Bcrypt Adapter', () => {
     await sut.hash('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a valid hash on success', async () => {
+    const salt = 12
+
+    const sut = new BcryptAdapter(salt)
+
+    const hash = await sut.hash('any_value')
+
+    expect(hash).toBe('hashed_value')
   })
 })
