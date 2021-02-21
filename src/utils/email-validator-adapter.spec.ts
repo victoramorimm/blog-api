bc7e4670102ecd211a56f5e8a8f7ab7048fd2a85
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-20 21:58:59 -0300

    test: ensure EmailValidator Adapter returns true on success

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,6 +1,12 @@
 import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator-adapter'
 
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
+
 describe('EmailValidator Adapter', () => {
   test('Should call isEmail with correct value', () => {
     const sut = new EmailValidatorAdapter()
@@ -10,5 +16,13 @@ describe('EmailValidator Adapter', () => {
     sut.validate('any_email@mail.com')
 
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return true on success', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isEmailValid = sut.validate('any_email@mail.com')
+
+    expect(isEmailValid).toBeTruthy()
   })
 })
