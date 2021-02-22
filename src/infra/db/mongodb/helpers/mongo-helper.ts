9088c0ac00f9017db6122e83050ea25d2aa5a454
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-22 16:30:13 -0300

    refactor: remove unnecessary variable

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,9 @@
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  uri: null as string,
   client: null as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    this.uri = uri
-
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
