069f6c0b42d82ce8451757743b51963ebf225241
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-23 22:12:14 -0300

    fix: remove duplicated code

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -10,7 +10,5 @@ export const adaptRoute = (controller: Controller) => {
     const httpResponse = await controller.handle(httpRequest)
 
     res.status(httpResponse.statusCode).json(httpResponse.body)
-
-    res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
