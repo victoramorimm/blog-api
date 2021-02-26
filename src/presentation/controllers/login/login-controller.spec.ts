f320292e3a960a0260e1610536bd5b23b69b07e8
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-25 21:47:25 -0300

    refactor: move fake requests creation to factory methods

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,16 +1,25 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http'
+import { HttpRequest } from '../../protocols'
 import { LoginController } from './login-controller'
+
+export const makeFakeRequestWithoutEmail = (): HttpRequest => ({
+  body: {
+    password: 'any_password'
+  }
+})
+
+export const makeFakeRequestWithoutPassword = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail'
+  }
+})
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
     const sut = new LoginController()
 
-    const httpResponse = await sut.handle({
-      body: {
-        password: 'any_password'
-      }
-    })
+    const httpResponse = await sut.handle(makeFakeRequestWithoutEmail())
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
@@ -18,11 +27,7 @@ describe('Login Controller', () => {
   test('Should return 400 if no password is provided', async () => {
     const sut = new LoginController()
 
-    const httpResponse = await sut.handle({
-      body: {
-        email: 'any_email@mail.com'
-      }
-    })
+    const httpResponse = await sut.handle(makeFakeRequestWithoutPassword())
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
