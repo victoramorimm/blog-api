525aea5286a29c098e0343b519404e373c420bdf
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-25 21:41:32 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -4,14 +4,14 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
-      return await new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError('email')))
-      )
-    } else {
-      return await new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError('password')))
-      )
+    const requiredFields = ['email', 'password']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return await new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError(field)))
+        )
+      }
     }
   }
 }
