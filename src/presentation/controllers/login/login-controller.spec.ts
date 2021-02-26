f0bd1661ce00e5459e64009301aba7b92d16d885
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-25 21:57:25 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,6 +1,6 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http'
-import { HttpRequest } from '../../protocols'
+import { EmailValidator, HttpRequest } from '../../protocols'
 import { LoginController } from './login-controller'
 
 export const makeFakeRequestWithoutEmail = (): HttpRequest => ({
@@ -11,13 +11,20 @@ export const makeFakeRequestWithoutEmail = (): HttpRequest => ({
 
 export const makeFakeRequestWithoutPassword = (): HttpRequest => ({
   body: {
-    email: 'any_email@mail'
+    email: 'any_email@mail.com'
+  }
+})
+
+export const makeFakeValidRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
   }
 })
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
+    const sut = new LoginController(null)
 
     const httpResponse = await sut.handle(makeFakeRequestWithoutEmail())
 
@@ -25,10 +32,28 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+    const sut = new LoginController(null)
 
     const httpResponse = await sut.handle(makeFakeRequestWithoutPassword())
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    class EmailValidatorStub implements EmailValidator {
+      validate(email: string): boolean {
+        return true
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub()
+
+    const validateSpy = jest.spyOn(emailValidatorStub, 'validate')
+
+    const sut = new LoginController(emailValidatorStub)
+
+    await sut.handle(makeFakeValidRequest())
+
+    expect(validateSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
