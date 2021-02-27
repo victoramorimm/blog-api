5dc80c60c7cc82fa7765ac99112707fce49edc08
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-27 17:47:08 -0300

    feat: ensure DbAuthentication calls Encrypter with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,13 +2,16 @@ import {
   Authentication,
   AuthenticationModel
 } from '../../../domain/usecases/authentication'
+import { Encrypter } from '../../protocols/criptography/encrypter'
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../add-account/db-add-account-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+    private readonly secret: string
   ) {}
 
   async authenticate(authenticationDate: AuthenticationModel): Promise<string> {
@@ -24,6 +27,8 @@ export class DbAuthentication implements Authentication {
       value: password,
       valueToCompare: account.password
     })
+
+    await this.encrypter.encrypt(account.id, this.secret)
 
     return null
   }
