aadefe9d15bb2c61a97002ccf64a1ae31a7ad372
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-02 15:14:13 -0300

    test: ensure PublicationController calls AddPublication with correct values

--- src/presentation/controllers/publication/publication-controller.spec.ts
+++ src/presentation/controllers/publication/publication-controller.spec.ts
@@ -1,14 +1,45 @@
+import { PublicationReturnedByDb } from '../../../domain/models/publication-returned-by-db'
+import { AddPublication } from '../../../domain/usecases/add-publication'
 import { MaximumOfCharacters, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http'
 import { PublicationController } from './publication-controller'
 
-const makeSut = (): PublicationController => {
-  return new PublicationController()
+const makeAddPublicationStub = (): AddPublication => {
+  class AddPublicationStub implements AddPublication {
+    async add(publication: string): Promise<PublicationReturnedByDb> {
+      const fakePublicationReturnedByDb: PublicationReturnedByDb = {
+        id: 'any_id',
+        publication: 'any_publication'
+      }
+
+      return await new Promise((resolve) =>
+        resolve(fakePublicationReturnedByDb)
+      )
+    }
+  }
+
+  return new AddPublicationStub()
+}
+
+type SutTypes = {
+  sut: PublicationController
+  addPublicationStub: AddPublication
+}
+
+const makeSut = (): SutTypes => {
+  const addPublicationStub = makeAddPublicationStub()
+
+  const sut = new PublicationController(addPublicationStub)
+
+  return {
+    sut,
+    addPublicationStub
+  }
 }
 
 describe('Publication Controller', () => {
   test('Should return 400 if no publication is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -24,7 +55,7 @@ describe('Publication Controller', () => {
   })
 
   test('Should return 400 if publication has more than 500 characters', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const publicationWithMoreThan500Characters = new Array(501 + 1).join(' ')
 
@@ -37,5 +68,21 @@ describe('Publication Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MaximumOfCharacters()))
+  })
+
+  test('Should call AddPublication with correct values', async () => {
+    const { sut, addPublicationStub } = makeSut()
+
+    const addSpy = jest.spyOn(addPublicationStub, 'add')
+
+    const httpRequest = {
+      body: {
+        publication: 'any_publication'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(addSpy).toHaveBeenCalledWith('any_publication')
   })
 })
