baecf577531bcbca02b74894b64333ee5973e977
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-02 15:36:35 -0300

    feat: ensure PublicationController returns 500 if AddPublication throws

--- src/presentation/controllers/publication/publication-controller.ts
+++ src/presentation/controllers/publication/publication-controller.ts
@@ -1,20 +1,28 @@
 import { AddPublication } from '../../../domain/usecases/publication/add-publication'
-import { MaximumOfCharacters, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http'
+import {
+  MaximumOfCharacters,
+  MissingParamError,
+  ServerError
+} from '../../errors'
+import { badRequest, serverError } from '../../helpers/http'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class PublicationController implements Controller {
   constructor(private readonly addPublication: AddPublication) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { publication } = httpRequest.body
+    try {
+      const { publication } = httpRequest.body
 
-    if (publication.length > 500) {
-      return badRequest(new MaximumOfCharacters())
+      if (publication.length > 500) {
+        return badRequest(new MaximumOfCharacters())
+      }
+
+      await this.addPublication.add(publication)
+
+      return badRequest(new MissingParamError('publication'))
+    } catch (error) {
+      return serverError(new ServerError())
     }
-
-    await this.addPublication.add(publication)
-
-    return badRequest(new MissingParamError('publication'))
   }
 }
