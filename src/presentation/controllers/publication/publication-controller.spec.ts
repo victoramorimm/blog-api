03c891e585e8273debc6450bb3e8faf85bf7e859
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-02 14:06:40 -0300

    test: ensure PublicationController returns 400 if publication has more than 500 characters

--- src/presentation/controllers/publication/publication-controller.spec.ts
+++ src/presentation/controllers/publication/publication-controller.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { MaximumOfCharacters, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http'
 import { PublicationController } from './publication-controller'
 
@@ -17,5 +17,21 @@ describe('Publication Controller', () => {
     expect(httpResponse).toEqual(
       badRequest(new MissingParamError('publication'))
     )
+  })
+
+  test('Should return 400 if publication has more than 500 characters', async () => {
+    const sut = new PublicationController()
+
+    const publicationWithMoreThan500Characters = new Array(501 + 1).join(' ')
+
+    const httpRequest = {
+      body: {
+        publication: publicationWithMoreThan500Characters
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MaximumOfCharacters()))
   })
 })
