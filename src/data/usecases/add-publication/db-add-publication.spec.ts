8ee4dde8fe0a98cfca63fe05b43e49a7f5cf7b8a
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-02 21:23:12 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-publication/db-add-publication.spec.ts
+++ src/data/usecases/add-publication/db-add-publication.spec.ts
@@ -1,27 +1,46 @@
 import { PublicationReturnedByDb } from '../../../domain/models/publication/publication-returned-by-db'
+import { AddPublication } from '../../../domain/usecases/publication/add-publication'
 import { AddPublicationRepository } from '../../protocols/db/publication/add-publication-repository'
 import { DbAddPublication } from './db-add-publication'
 
+const makeAddPublicationRepositoryStub = (): AddPublicationRepository => {
+  class AddPublicationRepositoryStub implements AddPublicationRepository {
+    async add(publication: string): Promise<PublicationReturnedByDb> {
+      const fakePublicationReturnedByDb = {
+        id: 'any_id',
+        publication: 'any_publication'
+      }
+
+      return await new Promise((resolve) =>
+        resolve(fakePublicationReturnedByDb)
+      )
+    }
+  }
+
+  return new AddPublicationRepositoryStub()
+}
+
+type SutTypes = {
+  sut: AddPublication
+  addPublicationRepositoryStub: AddPublicationRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addPublicationRepositoryStub = makeAddPublicationRepositoryStub()
+
+  const sut = new DbAddPublication(addPublicationRepositoryStub)
+
+  return {
+    sut,
+    addPublicationRepositoryStub
+  }
+}
+
 describe('DbAddPublication Usecase', () => {
   test('Should call AddPublicationRepository with correct value', async () => {
-    class AddPublicationRepositoryStub implements AddPublicationRepository {
-      async add(publication: string): Promise<PublicationReturnedByDb> {
-        const fakePublicationReturnedByDb = {
-          id: 'any_id',
-          publication: 'any_publication'
-        }
-
-        return await new Promise((resolve) =>
-          resolve(fakePublicationReturnedByDb)
-        )
-      }
-    }
-
-    const addPublicationRepositoryStub = new AddPublicationRepositoryStub()
+    const { sut, addPublicationRepositoryStub } = makeSut()
 
     const addSpy = jest.spyOn(addPublicationRepositoryStub, 'add')
-
-    const sut = new DbAddPublication(addPublicationRepositoryStub)
 
     await sut.add('any_publication')
 
