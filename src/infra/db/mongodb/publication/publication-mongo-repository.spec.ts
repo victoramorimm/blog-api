146184256e85b803f11a8fef8d58c056eff99458
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-05 19:36:37 -0300

    test: ensure PublicationMongoRepository returns a publication on add success

---
+++ src/infra/db/mongodb/publication/publication-mongo-repository.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { PublicationMongoRepository } from './publication-mongo-repository'
+
+describe('Publication Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const publicationCollection = await MongoHelper.getCollection(
+      'publications'
+    )
+
+    await publicationCollection.deleteMany({})
+  })
+
+  test('Should return a publication on add success', async () => {
+    const sut = new PublicationMongoRepository()
+
+    const publication = await sut.add('any_publication')
+
+    expect(publication).toBeTruthy()
+    expect(publication.id).toBeTruthy()
+    expect(publication.publication).toBe('any_publication')
+  })
+})
