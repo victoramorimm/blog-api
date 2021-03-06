ac55e3a8f145ae82e6d1cc1d312dcab0e6266f38
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-06 20:24:06 -0300

    test: ensure JwtAdapter returns a value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -60,5 +60,13 @@ describe('Jwt Adapter', () => {
 
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'any_secret')
     })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+
+      const value = await sut.decrypt('any_token')
+
+      expect(value).toBe('any_value')
+    })
   })
 })
