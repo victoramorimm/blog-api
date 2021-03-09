fd70631baedbfc9bf236524ab217d257b4534b4e
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-09 16:24:11 -0300

    feat: ensure LoadPublicationsController calls LoadPublications with correct value

--- src/presentation/controllers/publication/load-publications/load-publication-controller.ts
+++ src/presentation/controllers/publication/load-publications/load-publication-controller.ts
@@ -1,3 +1,4 @@
+import { LoadPublications } from '../../../../domain/usecases/publication/load-publications'
 import { MissingParamError } from '../../../errors'
 import { badRequest } from '../../../helpers/http'
 import {
@@ -6,9 +7,17 @@ import {
   HttpResponse
 } from '../add-publication/add-publication-protocols'
 
-export class LoadPublicationController implements Controller {
+export class LoadPublicationsController implements Controller {
+  constructor(private readonly loadPublications: LoadPublications) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = badRequest(new MissingParamError('userId'))
+    const { accountId } = httpRequest.params
+
+    if (accountId) {
+      await this.loadPublications.load(accountId)
+    }
+
+    const error = badRequest(new MissingParamError('accountId'))
 
     return await new Promise((resolve) => resolve(error))
   }
