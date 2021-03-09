f5ea92c01f2934a6a29d64b6fc539bb212606079
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-09 16:25:36 -0300

    test: ensure LoadPublicationsController calls LoadPublications with correct value

--- src/presentation/controllers/publication/load-publications/load-publications-controller.spec.ts
+++ src/presentation/controllers/publication/load-publications/load-publications-controller.spec.ts
@@ -1,15 +1,71 @@
+import { LoadPublications } from '../../../../domain/usecases/publication/load-publications'
 import { MissingParamError } from '../../../errors'
 import { badRequest } from '../../../helpers/http'
-import { LoadPublicationController } from './load-publication-controller'
+import {
+  HttpRequest,
+  PublicationReturnedByDb
+} from '../add-publication/add-publication-protocols'
+import { LoadPublicationsController } from './load-publication-controller'
+
+const makeFakePublicationArray = (): PublicationReturnedByDb[] => {
+  return [
+    {
+      id: 'any_id',
+      publication: 'any_publication'
+    },
+    {
+      id: 'another_id',
+      publication: 'another_publication'
+    }
+  ]
+}
 
 describe('LoadPublications Controller', () => {
-  test('Should return 400 if no userId is provided', async () => {
-    const sut = new LoadPublicationController()
+  test('Should return 400 if no accountId is provided', async () => {
+    class LoadPublicationsStub implements LoadPublications {
+      async load(accountId: string): Promise<PublicationReturnedByDb[]> {
+        const fakePublicationArray = makeFakePublicationArray()
 
-    const httpRequest = {}
+        return await new Promise((resolve) => resolve(fakePublicationArray))
+      }
+    }
+
+    const loadPublicationsStub = new LoadPublicationsStub()
+
+    const sut = new LoadPublicationsController(loadPublicationsStub)
+
+    const httpRequest = {
+      params: {}
+    }
 
     const httpResponse = await sut.handle(httpRequest)
 
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('userId')))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('accountId')))
+  })
+
+  test('Should call LoadPublications with correct value', async () => {
+    class LoadPublicationsStub implements LoadPublications {
+      async load(accountId: string): Promise<PublicationReturnedByDb[]> {
+        const fakePublicationArray = makeFakePublicationArray()
+
+        return await new Promise((resolve) => resolve(fakePublicationArray))
+      }
+    }
+
+    const loadPublicationsStub = new LoadPublicationsStub()
+
+    const loadSpy = jest.spyOn(loadPublicationsStub, 'load')
+
+    const sut = new LoadPublicationsController(loadPublicationsStub)
+
+    const httpRequest: HttpRequest = {
+      params: {
+        accountId: 'any_id'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(loadSpy).toHaveBeenCalledWith('any_id')
   })
 })
