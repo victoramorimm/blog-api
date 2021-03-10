025559ae2894aaa8194be7a74248f5202bbca5db
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-10 16:37:06 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/publication/load-publications/load-publications-controller.spec.ts
+++ src/presentation/controllers/publication/load-publications/load-publications-controller.spec.ts
@@ -7,30 +7,48 @@ import {
 } from '../add-publication/add-publication-protocols'
 import { LoadPublicationsController } from './load-publications-controller'
 
+const makeLoadPublicationsStub = (): LoadPublications => {
+  class LoadPublicationsStub implements LoadPublications {
+    async load(accountId: string): Promise<PublicationReturnedByDb[]> {
+      const fakePublications: PublicationReturnedByDb[] = [
+        {
+          id: 'any_id',
+          publication: 'any_publication',
+          accountId: 'any_id'
+        },
+        {
+          id: 'other_id',
+          publication: 'other_publication',
+          accountId: 'other_id'
+        }
+      ]
+
+      return fakePublications
+    }
+  }
+
+  return new LoadPublicationsStub()
+}
+
+type SutTypes = {
+  sut: LoadPublicationsController
+  loadPublicationsStub: LoadPublications
+}
+
+const makeSut = (): SutTypes => {
+  const loadPublicationsStub = makeLoadPublicationsStub()
+
+  const sut = new LoadPublicationsController(loadPublicationsStub)
+
+  return {
+    sut,
+    loadPublicationsStub
+  }
+}
+
 describe('LoadPublications Controller', () => {
   test('Should return 400 if no accountId is provided as param on url', async () => {
-    class LoadPublicationsStub implements LoadPublications {
-      async load(accountId: string): Promise<PublicationReturnedByDb[]> {
-        const fakePublications: PublicationReturnedByDb[] = [
-          {
-            id: 'any_id',
-            publication: 'any_publication',
-            accountId: 'any_id'
-          },
-          {
-            id: 'other_id',
-            publication: 'other_publication',
-            accountId: 'other_id'
-          }
-        ]
-
-        return fakePublications
-      }
-    }
-
-    const loadPublicationsStub = new LoadPublicationsStub()
-
-    const sut = new LoadPublicationsController(loadPublicationsStub)
+    const { sut } = makeSut()
 
     const httpRequest: HttpRequest = {
       params: {
@@ -44,30 +62,9 @@ describe('LoadPublications Controller', () => {
   })
 
   test('Should call LoadPublications with correct accountId', async () => {
-    class LoadPublicationsStub implements LoadPublications {
-      async load(accountId: string): Promise<PublicationReturnedByDb[]> {
-        const fakePublications: PublicationReturnedByDb[] = [
-          {
-            id: 'any_id',
-            publication: 'any_publication',
-            accountId: 'any_id'
-          },
-          {
-            id: 'other_id',
-            publication: 'other_publication',
-            accountId: 'other_id'
-          }
-        ]
-
-        return fakePublications
-      }
-    }
-
-    const loadPublicationsStub = new LoadPublicationsStub()
+    const { sut, loadPublicationsStub } = makeSut()
 
     const loadSpy = jest.spyOn(loadPublicationsStub, 'load')
-
-    const sut = new LoadPublicationsController(loadPublicationsStub)
 
     const httpRequest: HttpRequest = {
       params: {
