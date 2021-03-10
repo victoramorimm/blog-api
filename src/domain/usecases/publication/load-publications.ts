fefc4d66be6af9381a1004f830716f9e8fa0a304
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-10 15:46:21 -0300

    refactor: delete all about LoadPublicationsController

--- src/domain/usecases/publication/load-publications.ts
+++
@@ -1,5 +0,0 @@
-import { PublicationReturnedByDb } from '../../models/publication/publication-returned-by-db'
-
-export interface LoadPublications {
-  load: (accountId: string) => Promise<PublicationReturnedByDb[]>
-}
