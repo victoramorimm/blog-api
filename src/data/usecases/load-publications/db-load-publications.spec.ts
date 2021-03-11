ec70a5fddbd982137b26ab350a2dd92a12160723
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-11 14:49:50 -0300

    refactor: move sut and fake load-publications-repository-stub creation to factories methods

--- src/data/usecases/load-publications/db-load-publications.spec.ts
+++ src/data/usecases/load-publications/db-load-publications.spec.ts
@@ -2,34 +2,50 @@ import { PublicationReturnedByDbModel } from '../../models/publication-returned-
 import { LoadPublicationsRepository } from '../../protocols/db/publication/load-publications-repository'
 import { DbLoadPublications } from './db-load-publications'
 
+const makeFakeLoadPublicationsRepositoryStub = (): LoadPublicationsRepository => {
+  class LoadPublicationsRepositoryStub implements LoadPublicationsRepository {
+    async loadAll(accountId: string): Promise<PublicationReturnedByDbModel[]> {
+      const fakePublications: PublicationReturnedByDbModel[] = [
+        {
+          id: 'any_id',
+          accountId: 'any_id',
+          publication: 'any_publication'
+        },
+        {
+          id: 'other_id',
+          accountId: 'other_id',
+          publication: 'other_publication'
+        }
+      ]
+
+      return await new Promise((resolve) => resolve(fakePublications))
+    }
+  }
+
+  return new LoadPublicationsRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadPublications
+  loadPublicationsRepositoryStub: LoadPublicationsRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadPublicationsRepositoryStub = makeFakeLoadPublicationsRepositoryStub()
+
+  const sut = new DbLoadPublications(loadPublicationsRepositoryStub)
+
+  return {
+    sut,
+    loadPublicationsRepositoryStub
+  }
+}
+
 describe('DbLoadPublications Usecase', () => {
   test('Should call LoadPublicationsRepository with correct accountId', async () => {
-    class LoadPublicationsRepositoryStub implements LoadPublicationsRepository {
-      async loadAll(
-        accountId: string
-      ): Promise<PublicationReturnedByDbModel[]> {
-        const fakePublications: PublicationReturnedByDbModel[] = [
-          {
-            id: 'any_id',
-            accountId: 'any_id',
-            publication: 'any_publication'
-          },
-          {
-            id: 'other_id',
-            accountId: 'other_id',
-            publication: 'other_publication'
-          }
-        ]
-
-        return await new Promise((resolve) => resolve(fakePublications))
-      }
-    }
-
-    const loadPublicationsRepositoryStub = new LoadPublicationsRepositoryStub()
+    const { sut, loadPublicationsRepositoryStub } = makeSut()
 
     const loadAllSpy = jest.spyOn(loadPublicationsRepositoryStub, 'loadAll')
-
-    const sut = new DbLoadPublications(loadPublicationsRepositoryStub)
 
     await sut.load('any_id')
 
